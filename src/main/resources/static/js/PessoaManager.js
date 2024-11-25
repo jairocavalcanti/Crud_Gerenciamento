97041dd4635f893cb885b504dabb658dcd7f7333
RafaelBarcelos2109 <rafinhab2109@gmail.com>
2024-11-25 17:30:15 -0300

    Organizando Front-end

--- src/main/resources/static/js/PessoaManager.js
+++ src/main/resources/static/js/PessoaManager.js
@@ -1,27 +1,30 @@
-const baseUrl = 'http://localhost:8080/pessoas';  // URL da API para gerenciar as pessoas
+const baseUrl = 'http://localhost:8080/pessoas'; // URL da API
 
 // Função para buscar e exibir as pessoas
 async function fetchPessoas() {
     const response = await fetch(baseUrl);
     const pessoas = await response.json();
-    const pessoaList = document.getElementById('pessoa-list');
-    pessoaList.innerHTML = '';  // Limpa a lista de pessoas
+
+    const clienteList = document.getElementById('cliente-list');
+    const funcionarioList = document.getElementById('funcionario-list');
+    clienteList.innerHTML = '';
+    funcionarioList.innerHTML = '';
 
     pessoas.forEach(pessoa => {
         const listItem = document.createElement('li');
-        listItem.textContent = `ID: ${pessoa.id}, Nome: ${pessoa.nome}, Tipo: ${pessoa.tipo}`;
+        listItem.textContent = `ID: ${pessoa.id}, Nome: ${pessoa.nome}`;
 
         if (pessoa.tipo === 'Cliente') {
-            listItem.textContent += `, Telefone: ${pessoa.telefone}`; // Adiciona o telefone para clientes
+            listItem.textContent += `, Telefone: ${pessoa.telefone}`;
+            clienteList.appendChild(listItem);
         } else if (pessoa.tipo === 'Funcionario') {
-            listItem.textContent += `, Cargo: ${pessoa.cargo}`; // Adiciona o cargo para funcionários
+            listItem.textContent += `, Cargo: ${pessoa.cargo}`;
+            funcionarioList.appendChild(listItem);
         }
-
-        pessoaList.appendChild(listItem);
     });
 }
 
-// Carrega a lista de pessoas ao abrir a página
+// Carrega as listas ao abrir a página
 window.onload = () => {
     fetchPessoas();
 };
